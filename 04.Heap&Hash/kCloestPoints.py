77ccf9a9f5cfd674136a936345c22eab23e6159a
MingD <44249993+mjmingd@users.noreply.github.com>
2019-07-31 17:12:25 +0900

    Update kCloestPoints.py

--- 04.Heap&Hash/kCloestPoints.py
+++ 04.Heap&Hash/kCloestPoints.py
@@ -1,5 +1,6 @@
 # K Cloest Points to Origin
 # https://leetcode.com/problems/k-closest-points-to-origin/
+import heapq
 
 class Solution(object):
     def kClosest(self, points, K):
@@ -13,3 +14,20 @@ class Solution(object):
         
         return points[:K]
         
+        
+        
+#solution using heap
+    def kClosest2 (self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        # using min heap
+        heap = []
+        
+        for p in points:
+            d = p[0]** 2 + p[1]**2
+            heapq.heappush(heap, (d, p)) #(priority, value)
+            
+        return [heapq.heappop(heap)[1] for _ in range(K)]
