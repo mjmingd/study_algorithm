4aef6176a9cb3ed74e78785397a4bf246327ff8e
MingD <44249993+mjmingd@users.noreply.github.com>
2019-07-31 16:35:26 +0900

    Create kCloestPoints.py

---
+++ 04.Heap&Hash/kCloestPoints.py
@@ -0,0 +1,15 @@
+# K Cloest Points to Origin
+# https://leetcode.com/problems/k-closest-points-to-origin/
+
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        
+        points.sort(key = lambda p : p[0]**2+p[1]**2)
+        
+        return points[:K]
+        
