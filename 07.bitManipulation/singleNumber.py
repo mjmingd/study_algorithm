23eb18a10e96a85c1d460fc85aedb25108d10278
MingD <44249993+mjmingd@users.noreply.github.com>
2019-08-29 21:41:10 +0900

    Update singleNumber.py

--- 07.bitManipulation/singleNumber.py
+++ 07.bitManipulation/singleNumber.py
@@ -2,7 +2,23 @@
 # https://leetcode.com/problems/single-number/
 
 class Solution:
+    
     def singleNumber(self, nums: List[int]) -> int:
+    """
+    time complexcity : O(N)
+    space complexcity : O(1)
+    """
+        res = 0
+        for n in nums :
+            res ^= n
+        return res
+   
+    
+    def singleNumber2(self, nums: List[int]) -> int:
+    """
+    time complexcity : O(N)
+    space complexcity : O(N)
+    """
         ret = {}
         for n in nums :
             if n not in ret :
@@ -13,3 +29,14 @@ class Solution:
         for k,v in ret.items() :
             if v == 1 :
                 return k
+    
+    
+    def singleNumber1(self, nums: List[int]) -> int:
+        
+        ret = []
+        for n in nums :
+            if n not in ret :
+                ret.append(n)
+            else :
+                ret.remove(n)
+        return ret.pop()
