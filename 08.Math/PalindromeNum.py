48d8604d0a8decbefee0d06b1d645c46e6ab19a0
MingD <44249993+mjmingd@users.noreply.github.com>
2019-09-05 01:33:22 +0900

    Update PalindromeNum.py

--- 08.Math/PalindromeNum.py
+++ 08.Math/PalindromeNum.py
@@ -2,11 +2,25 @@
 # https://leetcode.com/problems/palindrome-number/
 
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-    """
-    time complexity :O(N)
-    space complexity : O(1)
-    """
+    def isPalindrome2(self, x: int) -> bool:
+        """
+        time complexity : O(log10(N))
+        space complexity : O(1)
+        """
+        
+        tmp, ans = x, 0
+        if x < 0 :
+            return False
+        while tmp > 0 :
+            ans = ans*10 + tmp % 10
+            tmp = tmp//10
+        return True if x == ans else False
+    
+    def isPalindrome1(self, x: int) -> bool:
+        """
+        time complexity :O(N)
+        space complexity : O(1)
+        """
         if x < 0 :
             return False
         x = str(x)
