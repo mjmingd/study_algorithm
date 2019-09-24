f929e71a3abfe4038d02b8ffc126ce6490f6d4d2
MingD <44249993+mjmingd@users.noreply.github.com>
2019-09-24 15:09:34 +0900

    Update climbingStairs.py

--- 09.Recursion and DP/climbingStairs.py
+++ 09.Recursion and DP/climbingStairs.py
@@ -3,6 +3,23 @@
 
 class Solution:
     def climbStairs(self, n: int) -> int:
+        '''
+        time complexity : O(N)
+        space complexity : O(1)
+        '''
+        
+        if n == 1:
+            return n
+        if n == 2 :
+            return 2
+
+        a, b = 1, 2
+        for i in range(2, n) :
+            c = a + b
+            a, b = b, c
+        return b
+            
+    def climbStairs2(self, n: int) -> int:
     '''
     time complexity : O(N)
     space complexity : O(N)
