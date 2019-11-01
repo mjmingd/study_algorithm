e0a1c8e7aa32fdb6fe9400bf8e824a22ec33242d
MingD <44249993+mjmingd@users.noreply.github.com>
2019-11-01 17:12:23 +0900

    Create 2-19.Add2Nums2.py

---
+++ 02.linked_list/2-19.Add2Nums2.py
@@ -0,0 +1,42 @@
+# Add Two Numbers II
+# https://leetcode.com/problems/add-two-numbers-ii/
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        '''
+        time complexity : O(N)
+        space complexity : O(1)
+        '''
+        
+        x, p1 = 0, l1
+        while p1 :
+            x = x*10 + p1.val
+            p1 = p1.next
+        
+        y, p2 = 0, l2
+        while p2 :
+            y = y*10 + p2.val
+            p2 = p2.next
+        
+        ans, dummy = x+y, ListNode(0)
+    
+        if ans == 0 : return dummy
+        while ans :
+            v, ans = ans%10, ans//10
+            dummy.next, dummy.next.next = ListNode(v), dummy.next
+            
+        return dummy.next
+
+            
+        
+            
+        
+        
+            
