8945697073905eb0f0058152888cd8fa6d6fbb42
MingD <44249993+mjmingd@users.noreply.github.com>
2019-11-13 17:05:38 +0900

    Create 2-26.houseRobber.py

---
+++ 09.Recursion and DP/2-26.houseRobber.py
@@ -0,0 +1,17 @@
+# House Robber
+# https://leetcode.com/problems/house-robber/
+
+from collections import defaultdict
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums : return 0
+        if len(nums) <= 2 : return max(nums)
+        
+        dp = defaultdict(int)
+        dp[0], dp[1] = 0 , nums[0]
+        
+        for i in range(len(nums)) :
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+            
+        return max(dp.values())
