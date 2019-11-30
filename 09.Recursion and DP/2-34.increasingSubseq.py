cf84c3d0dc5586ecf5c33221cc4aa55548578df9
MingD <44249993+mjmingd@users.noreply.github.com>
2019-11-30 12:52:42 +0900

    Update 2-34.increasingSubseq.py

--- 09.Recursion and DP/2-34.increasingSubseq.py
+++ 09.Recursion and DP/2-34.increasingSubseq.py
@@ -2,6 +2,28 @@
 # https://leetcode.com/problems/longest-increasing-subsequence/
 
 class Solution:
+    def lengthOfLIS2(self, nums: List[int]) -> int:
+        '''
+        time complexity : O(N^2)
+        space complexity : O(N)
+        '''
+        
+        if not nums : return 0
+        dp = [1 for _ in nums]
+        _len = len(nums)
+        
+        for i in range(_len) :
+            _max = 0
+            for j in range(i) :
+                if nums[j] < nums[i] :
+                    _max = max(dp[j], _max)
+            dp[i] = _max + 1
+        
+        return max(dp)
+    
+    
+    
+    
     def lengthOfLIS1(self, nums: List[int]) -> int:
     '''
     time complexity : TLE
